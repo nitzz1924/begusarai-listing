c76c8aa03140b681ce5453cdaefacbbad536e547
kishangopal <kishangopal.yuvmedia@gmail.com>
2023-09-05 12:53:37 +0530

    modal box issue

--- resources/views/backend/admin/master.blade.php
+++ resources/views/backend/admin/master.blade.php
@@ -1,45 +1,15 @@
+
 @extends('backend.layouts.master')
 @section('title', 'Dashboard')
 @section('content')
-    <div class="modal fade" id="basicModal" tabindex="-1">
-        <div class="modal-dialog">
-           
-          <div class="modal-content">
-            <form action="{{url('insert-master')}}" method="POST">
-            @csrf
-            <div class="modal-header">
-              <h5 class="modal-title">Add master category</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="row g-3">
-                    <div class="col-12">
-                        <label for="inputNanme4" class="form-label">Label</label>
-                        <input type="text" class="form-control" id="label" name="label" required />
-                    </div>
-                    <div class="col-12">
-                        <label for="inputEmail4" class="form-label">Value</label>
-                        <input type="text" name="value" class="form-control" id="value" required />
-                    </div>
-                </div>
-        
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-success">Add</button>
-            </div>
-            
-        </form>
-          </div>
-        </div>
-      </div><!-- End Scrolling Modal-->
+
 <div class="row">
         <div class="col-md-12">
             <div class="main-card mb-3 card">
                 <div class="card-header"><label><i class="fa fa-list ml-2"></i>Master List</label>
                     <div class="btn-actions-pane-right">
                         <div role="group" class="btn-group-sm btn-group">
-                        <button type="button" class="active btn btn-focus" data-bs-toggle="modal" data-bs-target="#basicModal"><i class="fa fa-plus"></i> Add master category</button>
+                        <button type="button" class="active btn btn-focus" data-bs-toggle="modal" data-bs-target="#mastermodelbox"><i class="fa fa-plus"></i> Add master category</button>
  
                         </div>
                     </div>
--- resources/views/backend/layouts/modal.blade.php
+++ resources/views/backend/layouts/modal.blade.php
@@ -4,7 +4,7 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <button type="button" class="close"  data-bs-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
             </div>
@@ -17,3 +17,35 @@
         </div>
     </div>
 </div>
+<div class="modal fade" id="mastermodelbox" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <form action="{{url('insert-master')}}" method="POST">
+            @csrf
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row g-3">
+                    <div class="col-12">
+                        <label for="inputNanme4" class="form-label">Label</label>
+                        <input type="text" class="form-control" id="label" name="label" required />
+                    </div>
+                    <div class="col-12">
+                        <label for="inputEmail4" class="form-label">Value</label>
+                        <input type="text" name="value" class="form-control" id="value" required />
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-success">Add</button>
+            </div>
+        </div>
+    </form>
+    </div>
+</div>
+   
